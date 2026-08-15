c2bc794d617b4c496d507415e146ac51af6f6f2f
sakuraba0028 <sakuraba0028@gmail.com>
2026-08-15 11:20:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// BEM対応
+// ケバブ・スネーク・BEM対応
 const toCamelCase = str => str.replace(/[-_]+([a-z])/g, (_, prefix) => prefix.toUpperCase());
 
 const getElementsByPrimaryClass = () => {
-    const classedElements = Array.from(document.querySelectorAll('*');
+    const classedElements = Array.from(document.querySelectorAll('*')).filter(el => el.className);
     const classedElementsRoster = classedElements.reduce((acc, el) => {
-        const camelPrimaryClass = el.classList[0];
+        const camelPrimaryClass = toCamelCase(el.classList[0]);
 
         acc[camelPrimaryClass] = el;
 
@@ -12,31 +12,4 @@ const getElementsByPrimaryClass = () => {
     });
 
     return classedElementsRoster;
-};
-
-const createGrid = (size = 256, line = 15) => {
-    if (line % 2 != 1) return;
-
-    const newGrid = document.createElement('canvas');
-    newGrid.width = size;
-    newGrid.height = size;
-
-    for (let i = 0; i < line; i++) {
-        const coord = i * size / (line + 1);
-        const isMedian = i === (line + 1) / 2;
-
-        const newXAxis = document.createElement('div');
-        newXAxis.style.left = `${coord}px`;
-        newXAxis.style.width = `${size}px`;
-        if (isMedian) newXAxis.style.borderColor = 'var(--red)';
-        newGrid.appendChild(newXAxis);
-
-        const newYAxis = document.createElement('div');
-        newYAxis.style.top = `${coord}px`;
-        newYAxis.style.height = `${size}px`;
-        if (isMedian) newXAxis.style.borderColor = 'var(--blue)';
-        newGrid.appendChild(newYAxis);
-    }
-
-    return newGrid;
 };
